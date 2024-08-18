ad37340854f2647ad8510949e4bfb622d78e8ea9
berdabo <juliankoh2023@gmail.com>
2024-08-18 10:44:53 +0800

    Setup files for basic tetris-browser layout

---
+++ app.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const grid = document.querySelector('.grid');
+let squares = Array.from(document.querySelectorAll('.grid div'));
+const ScoreDisplay = document.querySelector('#score');
+const StartBtn = document.querySelector('#start-button');
+const width = 10;
+
+
+
+const lTetromino = 'firstShape';
+const zTetromino = 'secondShape';
+const oTetromino = 'thirdshape';
+const iTetromino = 'fourthshape';
+const tTetromino = 'fifthshape';
+
+const tetrominoes = [lTetromino, zTetromino, oTetromino, iTetromino, tTetromino];
+
+
+})
