43a934672369ea59feb0e69969c5f60ff0a97060
cha <eve0110010010@gmail.com>
2024-08-19 20:54:20 +0800

    Add ability to draw tetromino on screen
    - Defined `drawTetromino` to draw specific tetromino on screen (Simply draws it on (0, 0), does not move nor has the ability to rotate yet)
    - Defined `getTetrominoByName` to get properties of specific tetromino given its name
    - Moved canvas element (`playfield`) and context (`ctx`) to outer scope
    - Made `drawGrid` flexible to accept width and height inputs
    - Defined `gridSize` constant at outer scope (for scalability)

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+// implementation mainly follows this guideline
+// https://tetris.fandom.com/wiki/Tetris_Guideline
+
+// gives a very good idea on how to make the game work on a basic level
+// https://gist.github.com/straker/3c98304f8a6a9174efd8292800891ea1
+
 document.addEventListener("DOMContentLoaded", () => {
 
-// Implementation guidelines
-// https://tetris.fandom.com/wiki/Tetris_Guideline
-// https://tetris.fandom.com/wiki/Playfield
+let playfield = document.getElementById("playfield");
+let ctx = playfield.getContext("2d");
 
 // https://tetris.fandom.com/wiki/SRS
 // tetrominos shape matrix
@@ -61,23 +66,46 @@ const colors = {
     "Z": "red"
 }
 
-// https://stackoverflow.com/a/64802566
+// how to draw grids on canva: https://stackoverflow.com/a/64802566
+// how the playfield should be: https://tetris.fandom.com/wiki/Playfield
 function drawGrid(width, height) {
-    let playfield = document.getElementById("playfield");
-    let ctx = playfield.getContext("2d");
-
-    const gridWidth = playfield.width / width;
-    const gridHeight = playfield.height / height;
+    playfield.width = width * gridSize;
+    playfield.height = height * gridSize;
 
     ctx.strokeStyle = "grey";
 
-    for (let x = 0; x <= playfield.width; x += gridWidth) {
-        for (let y = 0; y <= playfield.height; y += gridHeight)
-            ctx.strokeRect(x, y, gridWidth, gridHeight);
+    for (let x = 0; x <= playfield.width; x += gridSize) {
+        for (let y = 0; y <= playfield.height; y += gridSize)
+            ctx.strokeRect(x, y, gridSize, gridSize);
     }
 }
 
-// 10*24 grids
+// returns a tetromino along with its properties
+function getTetrominoByName(name) {
+    const tetromino = {
+        name: name,
+        matrix: tetrominos[name],
+        color: colors[name]
+    };
+    return tetromino;
+}
+
+// draws specific tetromino on screen (given input like "I", "Z")
+function drawTetromino(tetromino) {
+    ctx.fillStyle = tetromino.color;
+    for (let row = 0; row < tetromino.matrix.length; row++) {
+        for (let col = 0; col < tetromino.matrix[row].length; col++) {
+            if (tetromino.matrix[row][col]) {
+                ctx.fillRect(col * gridSize, row * gridSize, gridSize, gridSize);
+            }
+        }
+    }
+}
+
+// 10*24 grids with each of them sized 35
+const gridSize = 35;
 drawGrid(10, 24);
+
+drawTetromino(getTetrominoByName("L"));
 
 })
